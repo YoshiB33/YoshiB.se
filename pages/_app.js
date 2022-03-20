1d883aa21be532275e841e66fc47ded912935f8e
YoshiB33 <94956384+YoshiB33@users.noreply.github.com>
2022-03-20 13:20:33 +0100

    Add files via upload

---
+++ pages/_app.js
@@ -0,0 +1,265 @@
+"use strict";
+(() => {
+var exports = {};
+exports.id = 888;
+exports.ids = [888];
+exports.modules = {
+
+/***/ 881:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "default": () => (/* binding */ _app)
+});
+
+// EXTERNAL MODULE: external "react/jsx-runtime"
+var jsx_runtime_ = __webpack_require__(997);
+// EXTERNAL MODULE: ./node_modules/next/link.js
+var next_link = __webpack_require__(664);
+// EXTERNAL MODULE: external "react"
+var external_react_ = __webpack_require__(689);
+;// CONCATENATED MODULE: ./components/navbar.tsx
+
+
+
+const Navbar = ()=>{
+    const { 0: active , 1: setActive  } = (0,external_react_.useState)(false);
+    const handleClick = ()=>{
+        setActive(!active);
+    };
+    return(/*#__PURE__*/ jsx_runtime_.jsx(jsx_runtime_.Fragment, {
+        children: /*#__PURE__*/ (0,jsx_runtime_.jsxs)("nav", {
+            className: "flex items-center flex-wrap bg-slate-800 border-b border-b-slate-600 p-12",
+            children: [
+                /*#__PURE__*/ jsx_runtime_.jsx(next_link["default"], {
+                    href: "/",
+                    children: /*#__PURE__*/ jsx_runtime_.jsx("img", {
+                        src: "/Logo.svg",
+                        height: "400",
+                        width: "200"
+                    })
+                }),
+                /*#__PURE__*/ jsx_runtime_.jsx("button", {
+                    className: " inline-flex p-3 hover:bg-slate-600 rounded lg:hidden text-white ml-auto hover:text-white outline-none",
+                    onClick: handleClick,
+                    children: /*#__PURE__*/ jsx_runtime_.jsx("svg", {
+                        className: "w-6 h-6",
+                        fill: "none",
+                        stroke: "currentColor",
+                        viewBox: "0 0 24 24",
+                        xmlns: "http://www.w3.org/2000/svg",
+                        children: /*#__PURE__*/ jsx_runtime_.jsx("path", {
+                            strokeLinecap: "round",
+                            strokeLinejoin: "round",
+                            strokeWidth: 2,
+                            d: "M4 6h16M4 12h16M4 18h16"
+                        })
+                    })
+                }),
+                /*#__PURE__*/ jsx_runtime_.jsx("div", {
+                    className: `${active ? '' : 'hidden'}   w-full lg:inline-flex lg:flex-grow lg:w-auto`,
+                    children: /*#__PURE__*/ (0,jsx_runtime_.jsxs)("div", {
+                        className: "lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start flex flex-col lg:h-auto",
+                        children: [
+                            /*#__PURE__*/ jsx_runtime_.jsx(next_link["default"], {
+                                href: "/",
+                                children: /*#__PURE__*/ jsx_runtime_.jsx("a", {
+                                    className: "lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-slate-700 hover:text-white ",
+                                    children: "Home"
+                                })
+                            }),
+                            /*#__PURE__*/ jsx_runtime_.jsx(next_link["default"], {
+                                href: "/about",
+                                children: /*#__PURE__*/ jsx_runtime_.jsx("a", {
+                                    className: "lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-slate-700 hover:text-white",
+                                    children: "About"
+                                })
+                            })
+                        ]
+                    })
+                })
+            ]
+        })
+    }));
+};
+/* harmony default export */ const navbar = (Navbar);
+
+;// CONCATENATED MODULE: ./components/footer.tsx
+
+const Footer = ()=>{
+    return(/*#__PURE__*/ jsx_runtime_.jsx("div", {
+        className: " border-t border-t-slate-600 bg-slate-800",
+        children: /*#__PURE__*/ jsx_runtime_.jsx("h1", {
+            className: " p-12 m-t-70 text-center text-white font-body text-1xl",
+            children: "Copyright \xa9 YoshiB33"
+        })
+    }));
+};
+/* harmony default export */ const footer = (Footer);
+
+;// CONCATENATED MODULE: ./components/layout.jsx
+
+
+
+function Layout({ children  }) {
+    return(/*#__PURE__*/ (0,jsx_runtime_.jsxs)(jsx_runtime_.Fragment, {
+        children: [
+            /*#__PURE__*/ jsx_runtime_.jsx(navbar, {}),
+            /*#__PURE__*/ jsx_runtime_.jsx("main", {
+                className: "flex-1",
+                children: children
+            }),
+            /*#__PURE__*/ jsx_runtime_.jsx("div", {
+                className: "h-screen bg-slate-800 ",
+                children: /*#__PURE__*/ jsx_runtime_.jsx("link", {
+                    href: "/favicon.ico"
+                })
+            }),
+            /*#__PURE__*/ jsx_runtime_.jsx(footer, {})
+        ]
+    }));
+};
+
+;// CONCATENATED MODULE: ./pages/_app.tsx
+
+
+
+function MyApp({ Component , pageProps  }) {
+    return(/*#__PURE__*/ jsx_runtime_.jsx(Layout, {
+        children: /*#__PURE__*/ jsx_runtime_.jsx(Component, {
+            ...pageProps
+        })
+    }));
+}
+/* harmony default export */ const _app = (MyApp);
+
+
+/***/ }),
+
+/***/ 562:
+/***/ ((module) => {
+
+module.exports = require("next/dist/server/denormalize-page-path.js");
+
+/***/ }),
+
+/***/ 14:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/i18n/normalize-locale-path.js");
+
+/***/ }),
+
+/***/ 524:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/is-plain-object.js");
+
+/***/ }),
+
+/***/ 20:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/mitt.js");
+
+/***/ }),
+
+/***/ 964:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/router-context.js");
+
+/***/ }),
+
+/***/ 565:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/router/utils/get-asset-path-from-route.js");
+
+/***/ }),
+
+/***/ 365:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/router/utils/get-middleware-regex.js");
+
+/***/ }),
+
+/***/ 428:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/router/utils/is-dynamic.js");
+
+/***/ }),
+
+/***/ 292:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/router/utils/parse-relative-url.js");
+
+/***/ }),
+
+/***/ 979:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/router/utils/querystring.js");
+
+/***/ }),
+
+/***/ 52:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/router/utils/resolve-rewrites.js");
+
+/***/ }),
+
+/***/ 226:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/router/utils/route-matcher.js");
+
+/***/ }),
+
+/***/ 422:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/router/utils/route-regex.js");
+
+/***/ }),
+
+/***/ 232:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/utils.js");
+
+/***/ }),
+
+/***/ 689:
+/***/ ((module) => {
+
+module.exports = require("react");
+
+/***/ }),
+
+/***/ 997:
+/***/ ((module) => {
+
+module.exports = require("react/jsx-runtime");
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = __webpack_require__.X(0, [730,664], () => (__webpack_exec__(881)));
+module.exports = __webpack_exports__;
+
+})();
--- pages/api/protected-route.js
+++ pages/api/protected-route.js
@@ -1,7 +1,42 @@
-import { withApiAuthRequired, getSession } from '@auth0/nextjs-auth0';
+"use strict";
+(() => {
+var exports = {};
+exports.id = 306;
+exports.ids = [306];
+exports.modules = {
 
-export default withApiAuthRequired(function ProtectedRoute(req, res) {
+/***/ 93:
+/***/ ((module) => {
 
-  const session = getSession(req, res);
+module.exports = require("@auth0/nextjs-auth0");
 
-});
+/***/ }),
+
+/***/ 949:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var _auth0_nextjs_auth0__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(93);
+/* harmony import */ var _auth0_nextjs_auth0__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_auth0_nextjs_auth0__WEBPACK_IMPORTED_MODULE_0__);
+
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ((0,_auth0_nextjs_auth0__WEBPACK_IMPORTED_MODULE_0__.withApiAuthRequired)(function ProtectedRoute(req, res) {
+    const session = (0,_auth0_nextjs_auth0__WEBPACK_IMPORTED_MODULE_0__.getSession)(req, res);
+}));
+
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../../webpack-api-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = (__webpack_exec__(949));
+module.exports = __webpack_exports__;
+
+})();
